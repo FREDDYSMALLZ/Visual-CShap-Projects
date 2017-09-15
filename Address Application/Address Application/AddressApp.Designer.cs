aae5011eb89eac0f65905a9e9dd55e3d6f5d3592
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-09-15 06:29:56 -0500

    Initial commit

---
+++ Address Application/Address Application/AddressApp.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Address_Application
+{
+    partial class AddressApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.displayBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(45, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(45, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(45, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(45, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "City:";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(45, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zip Code:";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(45, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 33);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "State:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(197, 27);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(284, 35);
+            this.firstNameTextBox.TabIndex = 7;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(197, 86);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(284, 35);
+            this.lastNameTextBox.TabIndex = 8;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(197, 144);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(284, 35);
+            this.addressTextBox.TabIndex = 9;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(197, 197);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(284, 35);
+            this.cityTextBox.TabIndex = 10;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(197, 248);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(284, 35);
+            this.stateTextBox.TabIndex = 11;
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(197, 298);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(284, 35);
+            this.zipTextBox.TabIndex = 12;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(524, 27);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(530, 304);
+            this.dataGridView.TabIndex = 13;
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Location = new System.Drawing.Point(197, 399);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(146, 41);
+            this.insertBtn.TabIndex = 14;
+            this.insertBtn.Text = "Insert";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(349, 399);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(146, 41);
+            this.deleteBtn.TabIndex = 15;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(501, 399);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(146, 41);
+            this.updateBtn.TabIndex = 16;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // displayBtn
+            // 
+            this.displayBtn.Location = new System.Drawing.Point(653, 399);
+            this.displayBtn.Name = "displayBtn";
+            this.displayBtn.Size = new System.Drawing.Size(146, 41);
+            this.displayBtn.TabIndex = 17;
+            this.displayBtn.Text = "Display";
+            this.displayBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddressApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1082, 467);
+            this.Controls.Add(this.displayBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.zipTextBox);
+            this.Controls.Add(this.stateTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "AddressApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Address Window";
+            this.Load += new System.EventHandler(this.AddressApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button displayBtn;
+    }
+}
+
---
+++ Address Application/Address Application/AddressApp.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Address_Application
+{
+    public partial class AddressApp : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-OB9VPCC\SQLEXPRESS;Initial Catalog=AddressDB;Integrated Security=True");
+        public AddressApp()
+        {
+            InitializeComponent();
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "insert into AddressTBL values('" + firstNameTextBox.Text + "','"
+                    + lastNameTextBox.Text + "', '" + addressTextBox.Text + "','" + cityTextBox.Text + "', '"
+                    + stateTextBox.Text + "', '" + zipTextBox.Text + "')";
+                command.ExecuteNonQuery();
+                connection.Close();
+                disp_Data();
+
+                MessageBox.Show("The record has been Inserted Successfully. ");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error in Inserting the record. Please check your data entry or connection. ");
+            }
+
+        }
+        public void disp_Data()
+        {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "select * from AddressTBL";
+                    DataTable dataTable = new DataTable();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    dataGridView.DataSource = dataTable;
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Error in Display the record. Please check your data entry or connection. ");
+                }
+
+            }
+
+        private void AddressApp_Load(object sender, EventArgs e)
+        {
+            disp_Data();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "insert into AddressTBL values('" + firstNameTextBox.Text + "','"
+                    + lastNameTextBox.Text + "', '" + addressTextBox.Text + "','" + cityTextBox.Text + "', '"
+                    + stateTextBox.Text + "', '" + zipTextBox.Text + "')";
+                command.ExecuteNonQuery();
+                connection.Close();
+                disp_Data();
+
+                MessageBox.Show("The record has been Inserted Successfully. ");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error in Inserting the record. Please check your data entry or connection. ");
+            }
+        }
+    }
+    
+}
