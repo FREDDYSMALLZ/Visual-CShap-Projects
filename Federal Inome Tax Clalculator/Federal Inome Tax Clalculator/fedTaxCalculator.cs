493c2331d1306a108e610d991df27b924d9ea8f9
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-10-11 01:30:16 -0500

    commit

--- Federal Inome Tax Clalculator/Federal Inome Tax Clalculator/fedTaxCalculator.cs
+++ Federal Inome Tax Clalculator/Federal Inome Tax Clalculator/fedTaxCalculator.cs
@@ -44,151 +44,100 @@ namespace Federal_Inome_Tax_Clalculator
             {
                 status_Description = "Invalid inpute for computing/filing federal tax";
             }
+            /*
             double ten_percent = 10 / 100;
             double fifteen_percent = 15 / 100;
             double twenty_five_percent = 25 / 100;
             double twenty_eight_percent = 28 / 100;
             double thirty_three_percent = 33 / 100;
             double thirty_five_percent = 35 / 100;
-            double thirty_ninesix_percent = 39.6 / 100;
-
+            double thirty_ninesix_percent = 39.6 / 100;  
+          */
             double tax = 0;
 
-            if (status == 0)//Compute taxes for single filers
-            {
-                if (income <= 9325)
-                {
-                    tax = income * ten_percent;
-                }
-                else if (income <= 37950)
-                {
-                    tax = 9325 * ten_percent + (income - 9325) * fifteen_percent;
-                }
-                else if (income <= 91900)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (income - 37950) * twenty_five_percent;
-                }
-                else if (income <= 191650)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (91900 - 37950) * twenty_five_percent + (income - 91900) * twenty_eight_percent;
-                }
-                else if (income <= 416700)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (91900 - 37950) * twenty_five_percent + (191650 - 91900) * twenty_eight_percent
-                        + (income - 191650) * thirty_three_percent;
-                }
-                else if (income <= 418400)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (91900 - 37950) * twenty_five_percent + (191650 - 91900) * twenty_eight_percent
-                       + (416700 - 191650) * thirty_three_percent + (income - 416700) * thirty_five_percent;
-                }
+            if (status == 0)
+            { // Compute tax for single filers
+                if (income <= 8350)
+                    tax = income * 0.10;
+                else if (income <= 33950)
+                    tax = 8350 * 0.10 + (income - 8350) * 0.15;
+                else if (income <= 82250)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950)
+                      * 0.25;
+                else if (income <= 171550)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950)
+                      * 0.25 + (income - 82250) * 0.28;
+                else if (income <= 372950)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950)
+                      * 0.25 + (171550 - 82250) * 0.28 + (income - 171550)
+                      * 0.33;
                 else
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (91900 - 37950) * twenty_five_percent + (191650 - 91900) * twenty_eight_percent
-                      + (416700 - 191650) * thirty_three_percent + (418400 - 416700) * thirty_five_percent + (income - 418400) * thirty_ninesix_percent;
-                }
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950)
+                      * 0.25 + (171550 - 82250) * 0.28 + (372950 - 171550)
+                      * 0.33 + (income - 372950) * 0.35;
             }
-            else if (status == 1) //Compute tax for married file jointly or qualifying widow(er)
-            {
-                if (income <= 18650)
-                {
-                    tax = income * ten_percent;
-                }
-                else if (income <= 75900)
-                {
-                    tax = 18650 * ten_percent + (income - 18650) * fifteen_percent;
-                }
-                else if (income <= 153100)
-                {
-                    tax = 18650 * ten_percent + (75900 - 18650) * fifteen_percent + (income - 75900) * twenty_five_percent;
-                }
-                else if (income <= 233350)
-                {
-                    tax = 18650 * ten_percent + (75900 - 18650) * fifteen_percent + (153100 - 75900) * twenty_five_percent + (income - 153100) * twenty_eight_percent;
-                }
-                else if (income <= 416700)
-                {
-                    tax = 18650 * ten_percent + (75900 - 18650) * fifteen_percent + (153100 - 75900) * twenty_five_percent + (233350 - 153100) * twenty_eight_percent
-                        + (income - 233350) * thirty_three_percent;
-                }
-                else if (income <= 470700)
-                {
-                    tax = 18650 * ten_percent + (75900 - 18650) * fifteen_percent + (153100 - 75900) * twenty_five_percent + (233350 - 153100) * twenty_eight_percent
-                       + (416700 - 233350) * thirty_three_percent + (income - 416700) * thirty_five_percent;
-                }
+            else if (status == 1)
+            { // Compute tax for married file jointly
+                if (income <= 16700)
+                    tax = income * 0.10;
+                else if (income <= 67900)
+                    tax = 16700 * 0.10 + (income - 16700) * 0.15;
+                else if (income <= 137050)
+                    tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 67900)
+                      * 0.25;
+                else if (income <= 208850)
+                    tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900)
+                      * 0.25 + (income - 137050) * 0.28;
+                else if (income <= 372950)
+                    tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900)
+                      * 0.25 + (208850 - 137050) * 0.28 + (income - 208850)
+                      * 0.33;
                 else
-                {
-                    tax = 18650 * ten_percent + (75900 - 18650) * fifteen_percent + (153100 - 75900) * twenty_five_percent + (233350 - 153100) * twenty_eight_percent
-                      + (416700 - 233350) * thirty_three_percent + (470700 - 416700) * thirty_five_percent + (income - 470700) * thirty_ninesix_percent;
-                }
+                    tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900)
+                      * 0.25 + (208850 - 137050) * 0.28 + (372950 - 208850)
+                      * 0.33 + (income - 372950) * 0.35;
             }
-            else if (status == 2)//compute tax for Married separately
-            {
-                if (income <= 9325)
-                {
-                    tax = income * ten_percent;
-                }
-                else if (income <= 37950)
-                {
-                    tax = 9325 * ten_percent + (income - 9325) * fifteen_percent;
-                }
-                else if (income <= 76550)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (income - 37950) * twenty_five_percent;
-                }
-                else if (income <= 116675)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (76550 - 37950) * twenty_five_percent + (income - 76550) * twenty_eight_percent;
-                }
-                else if (income <= 208350)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (76550 - 37950) * twenty_five_percent + (116675 - 76550) * twenty_eight_percent
-                        + (income - 116675) * thirty_three_percent;
-                }
-                else if (income <= 235350)
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (76550 - 37950) * twenty_five_percent + (116675 - 76550) * twenty_eight_percent
-                       + (208350 - 116675) * thirty_three_percent + (income - 208350) * thirty_five_percent;
-                }
+            else if (status == 2)
+            { // Compute tax for married separately
+                if (income <= 8350)
+                    tax = income * 0.10;
+                else if (income <= 33950)
+                    tax = 8350 * 0.10 + (income - 8350) * 0.15;
+                else if (income <= 68525)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950)
+                      * 0.25;
+                else if (income <= 104425)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950)
+                      * 0.25 + (income - 68525) * 0.28;
+                else if (income <= 186475)
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950)
+                      * 0.25 + (104425 - 68525) * 0.28 + (income - 104425)
+                      * 0.33;
                 else
-                {
-                    tax = 9325 * ten_percent + (37950 - 9325) * fifteen_percent + (76550 - 37950) * twenty_five_percent + (116675 - 76550) * twenty_eight_percent
-                      + (208350 - 116675) * thirty_three_percent + (235350 - 208350) * thirty_five_percent + (income - 235350) * thirty_ninesix_percent;
-                }
+                    tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950)
+                      * 0.25 + (104425 - 68525) * 0.28 + (186475 - 104425)
+                      * 0.33 + (income - 186475) * 0.35;
             }
             else if (status == 3)
-            {
-                if (income <= 13350)
-                {
-                    tax = income * ten_percent;
-                }
-                else if (income <= 50800)
-                {
-                    tax = 13350 * ten_percent + (income - 13350) * fifteen_percent;
-                }
-                else if (income <= 131200)
-                {
-                    tax = 13350 * ten_percent + (50800 - 13350) * fifteen_percent + (income - 50800) * twenty_five_percent;
-                }
-                else if (income <= 212500)
-                {
-                    tax = 13350 * ten_percent + (50800 - 13350) * fifteen_percent + (131200 - 50800) * twenty_five_percent + (income - 131200) * twenty_eight_percent;
-                }
-                else if (income <= 416700)
-                {
-                    tax = 13350 * ten_percent + (50800 - 13350) * fifteen_percent + (131200 - 50800) * twenty_five_percent + (212500 - 131200) * twenty_eight_percent
-                        + (income - 212500) * thirty_three_percent;
-                }
-                else if (income <= 444550)
-                {
-                    tax = 13350 * ten_percent + (50800 - 13350) * fifteen_percent + (131200 - 50800) * twenty_five_percent + (212500 - 131200) * twenty_eight_percent
-                       + (416700 - 212500) * thirty_three_percent + (income - 416700) * thirty_five_percent;
-                }
+            { // Compute tax for head of household
+                if (income <= 11950)
+                    tax = income * 0.10;
+                else if (income <= 45500)
+                    tax = 11950 * 0.10 + (income - 11950) * 0.15;
+                else if (income <= 117450)
+                    tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (income - 45500)
+                      * 0.25;
+                else if (income <= 190200)
+                    tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500)
+                      * 0.25 + (income - 117450) * 0.28;
+                else if (income <= 372950)
+                    tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500)
+                      * 0.25 + (190200 - 117450) * 0.28 + (income - 190200)
+                      * 0.33;
                 else
-                {
-                    tax = 13350 * ten_percent + (50800 - 13350) * fifteen_percent + (131200 - 50800) * twenty_five_percent + (212500 - 131200) * twenty_eight_percent
-                      + (416700 - 212500) * thirty_three_percent + (444550 - 416700) * thirty_five_percent + (income - 444550) * thirty_ninesix_percent;
-                }
+                    tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500)
+                      * 0.25 + (190200 - 117450) * 0.28 + (372950 - 190200)
+                      * 0.33 + (income - 372950) * 0.35;
             }
             else
             {
