bf62c2127c76521ce836c45a9db1cf40b173a178
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-03-03 15:14:44 -0600

    Uploads

---
+++ Creating_Arrays/Creating_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Creating_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {32,27,64,18,95}; //An array which contains five elements
+
+            Console.WriteLine($"{"Index"}{"Value",8}");
+
+            //Output for each aaray ane emlements value
+            for (int counter = 0; counter < array.Length; ++counter)
+            {
+                Console.WriteLine($"{counter,5} {array[counter],8}");
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
