ffcc676cf90a9c319809ce30f0a549944fe92dc3
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-03-03 14:32:38 -0600

    Inititial Commit

---
+++ StudentScoreMultiD/StudentScoreMultiD/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentScoreMultiD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] student = new int[3][];
+            student[0] = new int[2] { 56, 76 };
+            student[1] = new int[1] { 83};
+            student[2] = new int[3] {34,78,67};
+
+            double sum;
+            for (int i = 0; i < student.Length; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < student[i].Length; j++)
+                {
+                    sum += student[i][j];
+
+
+                    Console.Write("The Average score for student {0} is  ", i);
+
+                    Console.WriteLine((sum/student[i].Length).ToString("F1"));
+
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+    }
+}
