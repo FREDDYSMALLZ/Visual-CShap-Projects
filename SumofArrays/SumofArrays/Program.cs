72f41d22e93ed2436afe07303344f0913c7a3790
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-03-03 16:07:52 -0600

    Inititial commit

---
+++ SumofArrays/SumofArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumofArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 87, 68, 94, 100, 83, 78, 81, 95, 76, 87 };
+            int total = 0;
+            //add each elements's value to the total
+            /* for (int counter = 0; counter < array.Length; ++counter)           
+             {
+                 total += array[counter]; //ad element value to the total
+
+             }
+             */
+            foreach (int number in array)
+            {
+                total += number;
+            }
+
+            Console.WriteLine($"Total of Array elements: {total}");
+            Console.WriteLine("The totals for the arrays elemts is {0} ", total);
+
+            Console.ReadLine();
+        }
+    }
+}
