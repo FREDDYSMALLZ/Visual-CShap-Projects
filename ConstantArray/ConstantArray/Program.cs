b3e1517a9b50934c6dfedeacfd1c4605fef5a572
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-03-03 15:49:15 -0600

    Initial commit for a constant array

---
+++ ConstantArray/ConstantArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConstantArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ArrayLength = 5;
+            int[] array = new int[ArrayLength];
+
+
+
+            //Output for each aaray ane emlements value
+            for (int counter = 0; counter < array.Length; ++counter)
+            {
+                array[counter] = 2 + 2 * counter;
+            }
+
+                Console.WriteLine($"{"Index"}{"Value",8}"); //Headings
+                for (int counter = 0; counter < array.Length; ++counter)
+                {
+                    Console.WriteLine($"{counter,5} {array[counter],8}");
+                }              
+
+                Console.ReadLine();
+            }
+        }
+    }
+
