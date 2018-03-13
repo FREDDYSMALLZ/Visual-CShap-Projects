ed946f9620481b5fee74400c3c59e0403eb81c6c
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-03-13 02:14:13 -0500

    Inititial commit

---
+++ Trapezium_Getter_Setter/Trapezium_Getter_Setter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapezium_Getter_Setter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the longest length (a) of the Trapezium: ");
+            var length_a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the short length (b) of the Trapezium: ");
+            var length_b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the height (h) of the Trapezium: ");
+            var height = Convert.ToDouble(Console.ReadLine());
+
+            var myTrapezium = new Trapezium(length_a, length_b, height);
+            Display_Trapezium_Area(myTrapezium);//Mthod to get the area of the Trapezium
+
+            Console.WriteLine("Press Enter to Exit");
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void Display_Trapezium_Area(Trapezium myTrapezium)
+        {
+            Console.WriteLine($"The Long length of the trapezium is   : { myTrapezium.Length_a} meters.   ");
+            Console.WriteLine($"The Short length of the trapezium is   : { myTrapezium.Length_b} meters.   ");
+            Console.WriteLine($"The Height of the trapezium is   : { myTrapezium.Height} meters.   ");
+
+            Console.WriteLine($"Therefore, the Area of the trapezium is   : {myTrapezium.Area_trapezium() } square meters.");
+        }
+    }
+}
---
+++ Trapezium_Getter_Setter/Trapezium_Getter_Setter/Trapezium.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapezium_Getter_Setter
+{
+    class Trapezium
+    {
+            private double length_a;
+            private double length_b;
+            private double height;
+
+            public Trapezium(double length_a, double length_b, double height)
+            {
+                this.length_a = length_a;
+                this.length_b = length_b;
+                this.height = height;
+            }
+
+            public double Length_a
+            {
+                get { return length_a; }
+            }
+            public double Length_b
+            {
+                get { return length_b; }
+            }
+            public double Height
+            {
+                get { return height; }
+            }
+
+            public double Area_trapezium()
+            {
+                return 0.5 * (length_a + length_b) * height;
+            }
+        
+    }
+}
