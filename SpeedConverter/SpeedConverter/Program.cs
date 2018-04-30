ac8aafeca92fcd7ff426b83843e7cf670a6a5c7c
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-04-29 21:47:02 -0700

    Commit

---
+++ SpeedConverter/SpeedConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace SpeedConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter the Speed in Miles per hour: ");
+            var speed = Convert.ToDouble(ReadLine());
+
+            string value = $"Enter the distance traveled in miles: ";
+            WriteLine(value);
+            var distance = Convert.ToDouble(ReadLine());
+
+            var time = distance / speed;
+
+
+            WriteLine("It takes {0} time to travel {1} " +
+                              "miles at the speed of {2} " +
+                              "miles per hour",time,distance,speed);
+
+        }
+    }
+}
