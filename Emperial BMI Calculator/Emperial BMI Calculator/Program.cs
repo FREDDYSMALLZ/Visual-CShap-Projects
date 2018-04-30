62315d5899897a538208fcdea05227363233ce22
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-04-29 17:00:20 -0700

    commit

--- Emperial BMI Calculator/Emperial BMI Calculator/Program.cs
+++ Emperial BMI Calculator/Emperial BMI Calculator/Program.cs
@@ -2,37 +2,34 @@
 
 namespace Emperial_BMI_Calculator
 {
-   public class BMICalculator
+   public class BmiCalculator
     {
         static void Main(string[] args)
         {
-            double weight_in_pounds;
-            double height_in_inches;
-
             Console.Write("Please Enter the weight in pounds:  ");
-            String Input = Console.ReadLine();
-            weight_in_pounds = double.Parse(Input);
+            String input = Console.ReadLine();
+            var weightInPounds = double.Parse(input);
 
 
             Console.Write("Please enter the height in inches:  ");
-            String Input2 = Console.ReadLine();
-            height_in_inches = double.Parse(Input2);
+            String input2 = Console.ReadLine();
+            var heightInInches = double.Parse(input2);
 
-            double TOTAL_INCHES = height_in_inches * 12;
+            double totalInches = heightInInches * 12;
 
-            double BODY_MASS_INDEX = (weight_in_pounds / (Math.Pow(TOTAL_INCHES, 2.0)) * 703);
+            double bodyMassIndex = (weightInPounds / (Math.Pow(totalInches, 2.0)) * 703);
 
-            Console.WriteLine("The Body Mass Index is {0}", BODY_MASS_INDEX);
+            Console.WriteLine("The Body Mass Index is {0}", bodyMassIndex);
 
-            if (BODY_MASS_INDEX < 18.5)
+            if (bodyMassIndex < 18.5)
             {
                 Console.WriteLine("The Person is Underweight");
             }
-            else if (BODY_MASS_INDEX < 25.0)
+            else if (bodyMassIndex < 25.0)
             {
                 Console.WriteLine("The Person Has Normal Weight");
             }
-            else if (BODY_MASS_INDEX < 30.0)
+            else if (bodyMassIndex < 30.0)
             {
                 Console.WriteLine("The Person is Overweight");
             }
@@ -40,6 +37,7 @@ namespace Emperial_BMI_Calculator
             {
                 Console.WriteLine("The Person is Obese..!");
             }
+
             Console.ReadLine();
 
            
