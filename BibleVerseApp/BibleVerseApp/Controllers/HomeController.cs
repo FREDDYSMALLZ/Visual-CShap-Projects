94d29b561706ae26b37e109584d0f25a8fc47d5e
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2018-09-03 21:14:44 -0600

    Initial Application Commit

---
+++ BibleVerseApp/BibleVerseApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibleVerseApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult BibleDirectory()
+        {
+            ViewBag.Message = "This is your Bible Directory Page.";
+            return View();
+        }
+    }
+}
---
+++ BibleVerseApp/BibleVerseApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BibleVerseApp.Startup))]
+namespace BibleVerseApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
