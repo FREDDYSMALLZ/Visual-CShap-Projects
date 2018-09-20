9b48060409415535970bb1d2d3465b38cc44985c
Fredrick Onduso Ondieki <freddieondieki@outlook.com>
2018-09-20 14:32:35 -0500

    Updated the Routes file for the App to start with the Login Page

--- BibleVerseApplication/BibleVerseApplication/App_Start/RouteConfig.cs
+++ BibleVerseApplication/BibleVerseApplication/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace BibleVerseApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
